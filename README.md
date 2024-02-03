713b9ac8f1c9585f913e6b5accbb04e1a6108df8
Stefan Kecskes <mr.kecskes@gmail.com>
2024-02-03 23:07:25 +0000

    add temporary data

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # Docker volumes
 
+Run locally:
+
+    pip install -r requirements.txt
+    uvicorn main:app --host=0.0.0.0 --port=8080
 
 Create docker image:
     
     docker build -t myapp .
-
 
 Run docker image:
 
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
+from starlette.staticfiles import StaticFiles
 
 app = FastAPI()
-
+app.mount("/names", StaticFiles(directory="names", html=True), name="names")
 
 @app.get("/")
 async def root():
@@ -10,4 +11,7 @@ async def root():
 
 @app.get("/hello/{name}")
 async def say_hello(name: str):
+
+    with open(f"names/welcomed.txt", "at+") as file:
+        file.write(f"{name}\n")
     return {"message": f"Hello {name}"}
